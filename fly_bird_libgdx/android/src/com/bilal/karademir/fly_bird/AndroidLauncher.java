1074a24ed206bf884a8d75b18b0d8d1fc69faae9
Bilal Karademir <sempatikcerkez@gmail.com>
2019-09-01 16:32:33 +0300

    initial commit

---
+++ fly_bird_libgdx/android/src/com/bilal/karademir/fly_bird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bilal.karademir.fly_bird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bilal.karademir.fly_bird.FlyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlyBird(), config);
+	}
+}
